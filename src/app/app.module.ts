90aa4e1dcbb78fe27c2810b3b088264b9ca5c888
Ramon Cardoso <96ddffab-b9f9-464c-87e4-c0a8e4e9c35d@mon.anonaddy.com>
2020-07-22 07:04:13 -0300

    Angular Router Basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -19,7 +23,10 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -28,7 +35,8 @@ import { FooterComponent } from './footer/footer.component';
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    AppRoutingModule
   ],
   providers: [DishService],
   bootstrap: [AppComponent]
