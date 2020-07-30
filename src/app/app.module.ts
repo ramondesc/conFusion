b58555c2624bbd6ac5b86ef23c9687aa4676b158
Ramon Cardoso <96ddffab-b9f9-464c-87e4-c0a8e4e9c35d@mon.anonaddy.com>
2020-07-29 22:00:41 -0300

    Template Forms Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,10 @@ import { LeaderService } from './services/leader.service';
 import { LoginComponent } from './login/login.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { PromotionService } from './services/promotion.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -42,7 +46,11 @@ import { PromotionService } from './services/promotion.service';
     MatCardModule,
     MatButtonModule,
     AppRoutingModule,
-    MatDialogModule
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    FormsModule
   ],
   entryComponents: [
     LoginComponent
